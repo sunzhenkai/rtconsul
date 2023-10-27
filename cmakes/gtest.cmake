bf45b86be7db418b99c7e7474c58fbf19c085f3a
zhenkai.sun <zhenkai.sun@qq.com>
2023-10-27 11:06:57 +0800

    fix test

--- cmakes/gtest.cmake
+++ cmakes/gtest.cmake
@@ -1,3 +1,18 @@
-file(GLOB G_TESTS_SRC "${CMAKE_SOURCE_DIR}/tests/gtest/tests*.cpp")
-add_executable(rtcfg_g_tests tests/gtest/main.cpp ${SRCS_DIRS} ${G_TESTS_SRC})
-target_link_libraries(rtcfg_g_tests ${EXTRA} googletest::gtest)
+ENABLE_TESTING()
+FIND_PACKAGE(GTest CONFIG REQUIRED)
+
+SET(GTESTS_SRCS ${PRJ_SRCS})
+AUX_SOURCE_DIRECTORY(${PROJECT_SOURCE_DIR}/tests/gtests GTESTS_SRCS)
+ADD_EXECUTABLE(GTestMain ${GTESTS_SRCS})
+SET(GTEST_LIBS GTest::gtest GTest::gtest_main GTest::gmock GTest::gmock_main)
+TARGET_LINK_LIBRARIES(GTestMain PRIVATE ${GTEST_LIBS} ${DEP_LIBS})
+
+ADD_TEST(AllTestsInMain GTestMain)
+
+ADD_CUSTOM_COMMAND(
+        TARGET GTestMain
+        POST_BUILD
+        COMMAND mkdir -p ${CMAKE_BINARY_DIR}/log
+        # COMMAND mkdir -p ${CMAKE_BINARY_DIR}/tests/assets
+        # COMMAND cp -rf ${PROJECT_SOURCE_DIR}/tests/assets/* ${CMAKE_BINARY_DIR}/tests/assets/
+)
--- cmakes/test.cmake
+++ cmakes/test.cmake
@@ -1,5 +1,5 @@
-add_executable(test tests/unit/test.cpp ${SRCS_DIRS})
-target_link_libraries(test ${EXTRA})
+add_executable(test_main tests/unit/test.cpp ${SRCS_DIRS})
+target_link_libraries(test_main ${EXTRA})
 
 add_executable(test_service tests/unit/test_service.cpp ${SRCS_DIRS})
 target_link_libraries(test_service ${EXTRA})
